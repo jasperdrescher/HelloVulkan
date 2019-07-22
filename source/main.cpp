d8ad060ec7af02f56a14ed2f2f2a0fce33c4afcc
JasperDre <jasperdre@gmail.com>
2019-07-22 15:41:06 +0200

    Add Vulkan

--- source/main.cpp
+++ source/main.cpp
@@ -19,6 +19,8 @@ int main(int argc, char** argv)
 	uint32_t extensionCount = 0;
 	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
+	std::cout << extensionCount << " extensions supported" << std::endl;
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
