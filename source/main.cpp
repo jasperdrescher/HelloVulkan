22845f2ede6e610a5ef9f3a0da2480a5946abb1b
JasperDre <jasperdre@gmail.com>
2019-07-22 10:07:00 +0200

    Create main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,29 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+int main(int argc, char** argv)
+{
+	GLFWwindow* window;
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	window = glfwCreateWindow(1240, 720, "Hello Vulkan", nullptr, nullptr);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
+	return 0;
+}
