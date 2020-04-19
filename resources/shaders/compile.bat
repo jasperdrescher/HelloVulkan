f61bec31148c04d9d67f881c26c06aa46614cf44
JasperDre <jasperdre@gmail.com>
2020-04-19 13:31:59 +0200

    Update shader compiler

--- resources/shaders/compile.bat
+++ resources/shaders/compile.bat
@@ -1,3 +1,3 @@
-C:/VulkanSDK/1.1.108.0/Bin32/glslangValidator.exe -V ../shaders/shader.vert
-C:/VulkanSDK/1.1.108.0/Bin32/glslangValidator.exe -V ../shaders/shader.frag
+C:/VulkanSDK/1.2.131.2/Bin32/glslangValidator.exe -V ../shaders/shader.vert
+C:/VulkanSDK/1.2.131.2/Bin32/glslangValidator.exe -V ../shaders/shader.frag
 pause
